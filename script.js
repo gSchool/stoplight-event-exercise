e872b02c1fa0493fe2e951723ceffe2980f4b9af
LKHohn <lkhohn@gmail.com>
2015-11-12 14:21:47 -0700

    lindsay commit

--- script.js
+++ script.js
@@ -8,3 +8,37 @@
   When I click on the 'go' button
     the bottom light should turn green.
 */
+
+// var container = document.getElementById('container');
+// function eventHandler (event) {
+//  console.log('p clicked!');
+// }
+// container.addEventListener('click', eventHandler)
+
+
+//Stop Button
+var makeRed = document.getElementById('stopButton');
+function changeColorRed (click){
+  document.getElementById('stopLight').style.backgroundColor='red';
+  document.getElementById('slowLight').style.backgroundColor='black';
+  document.getElementById('gopLight').style.backgroundColor='black';
+}
+makeRed.addEventListener('click', changeColorRed);
+
+//Slow Button
+var makeYellow = document.getElementById('slowButton');
+function changeColorYellow (click){
+  document.getElementById('slowLight').style.backgroundColor='yellow';
+  document.getElementById('goLight').style.backgroundColor='black';
+  document.getElementById('stopLight').style.backgroundColor='black';
+}
+makeYellow.addEventListener("click", changeColorYellow);
+
+//Go Button
+var makeGreen = document.getElementById('goButton');
+function changeColorGreen (click){
+  document.getElementById('goLight').style.backgroundColor='green';
+  document.getElementById('stopLight').style.backgroundColor='black';
+  document.getElementById('slowLight').style.backgroundColor='black';
+}
+makeGreen.addEventListener("click", changeColorGreen);
