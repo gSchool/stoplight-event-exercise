999bd0724da249e2c4f3228af7f6c8c94cbb152f
Ethan Weeks <ethanweeks@ymail.com>
2015-11-12 16:09:04 -0700

    stop light functionial

--- script.js
+++ script.js
@@ -8,3 +8,35 @@
   When I click on the 'go' button
     the bottom light should turn green.
 */
+
+// red light
+var stopLit = document.getElementById('stopLight');
+var stopBut = document.getElementById('stopButton');
+// orange light
+var slowLit = document.getElementById('slowLight');
+var slowBut = document.getElementById('slowButton');
+// green light
+var goLit = document.getElementById('goLight');
+var goBut =document.getElementById('goButton');
+
+function black(){
+	stopLit.style['background'] = 'black';
+	slowLit.style['background'] = 'black';
+	goLit.style['background'] = 'black';
+};
+
+
+stopBut.addEventListener('click', function() {
+	stopLit.style['background'] = 'red';
+	setTimeout(black, 1500);
+});
+
+slowBut.addEventListener('click', function() {
+	slowLit.style['background'] = 'orange';
+	setTimeout(black, 1500);
+});
+
+goBut.addEventListener('click', function(){
+	goLit.style['background'] = 'green';
+	setTimeout(black, 1500);
+});
