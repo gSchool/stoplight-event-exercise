e491518c46c16bc8f32e80b1ca5c47e5506e5a04
Jeremy Roelfs <jeremyroelfs@icloud.com>
2015-11-12 14:05:31 -0700

    jeremy-stoplight

--- script.js
+++ script.js
@@ -8,3 +8,40 @@
   When I click on the 'go' button
     the bottom light should turn green.
 */
+
+var red = false;
+var redBtn = document.querySelector("#stopButton");
+redBtn.addEventListener('click',function(){
+	var light = document.querySelector("#stopLight");
+	if (!red) {
+		light.style.backgroundColor = 'red';
+	} else {
+		light.style.backgroundColor = 'black';
+	}
+	red = !red;
+	
+});
+
+var yellow = false;
+var yellowBtn = document.querySelector("#slowButton");
+yellowBtn.addEventListener('click',function(){
+	var light = document.querySelector("#slowLight");
+	if (!yellow) {
+		light.style.backgroundColor = 'yellow';
+	} else {
+		light.style.backgroundColor = 'black';
+	}
+	yellow = !yellow;
+});
+
+var green = false;
+var greenBtn = document.querySelector("#goButton");
+greenBtn.addEventListener('click',function(){
+	var light = document.querySelector("#goLight");
+	if (!green) {
+		light.style.backgroundColor = 'green';
+	} else {
+		light.style.backgroundColor = 'black';
+	}
+	green = !green;
+});
