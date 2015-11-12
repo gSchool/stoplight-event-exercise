46508376e0d55471b912d5fe961604188221956d
Jeremy Roelfs <jeremyroelfs@icloud.com>
2015-11-12 16:39:41 -0700

    jeremy-update

--- script.js
+++ script.js
@@ -9,39 +9,92 @@
     the bottom light should turn green.
 */
 
-var red = false;
-var redBtn = document.querySelector("#stopButton");
-redBtn.addEventListener('click',function(){
-	var light = document.querySelector("#stopLight");
-	if (!red) {
-		light.style.backgroundColor = 'red';
-	} else {
-		light.style.backgroundColor = 'black';
-	}
-	red = !red;
+var MyProgram = function(){
+
+	var red = false;
+	var yellow = false;
+	var green = false;
 	
-});
+	var redBtn = document.querySelector("#stopButton");
+	redBtn.addEventListener('click',function(){
+		red = true;
+		yellow = false;
+		green = false;
+		lightChange();
+	});
+	
+	var yellowBtn = document.querySelector("#slowButton");
+	yellowBtn.addEventListener('click',function(){
+		red = false;
+		yellow = true;
+		green = false;
+		lightChange();
+	});
 
-var yellow = false;
-var yellowBtn = document.querySelector("#slowButton");
-yellowBtn.addEventListener('click',function(){
-	var light = document.querySelector("#slowLight");
-	if (!yellow) {
-		light.style.backgroundColor = 'yellow';
-	} else {
-		light.style.backgroundColor = 'black';
-	}
-	yellow = !yellow;
-});
+	var greenBtn = document.querySelector('#goButton');
+	greenBtn.addEventListener('click',function(){
+		red = false;
+		yellow = false;
+		green = true;
+		lightChange();
+	});
 
-var green = false;
-var greenBtn = document.querySelector("#goButton");
-greenBtn.addEventListener('click',function(){
-	var light = document.querySelector("#goLight");
-	if (!green) {
-		light.style.backgroundColor = 'green';
-	} else {
-		light.style.backgroundColor = 'black';
-	}
-	green = !green;
-});
+
+function lightChange() {
+
+      if(red) {
+      		document.querySelector('#goLight').style.backgroundColor = 'black';
+      		document.querySelector('#slowLight').style.backgroundColor = 'black';
+      		document.querySelector('#stopLight').style.backgroundColor = 'red';
+      		red = false;
+			yellow = true;
+			green = false;
+      } else if(yellow) {
+      		document.querySelector('#goLight').style.backgroundColor = 'black';
+      		document.querySelector('#slowLight').style.backgroundColor = 'yellow';
+      		document.querySelector('#stopLight').style.backgroundColor = 'black';
+      		red = false;
+			yellow = false;
+			green = true;
+      } else if(green) {
+      		document.querySelector('#goLight').style.backgroundColor = 'green';
+      		document.querySelector('#slowLight').style.backgroundColor = 'black';
+      		document.querySelector('#stopLight').style.backgroundColor = 'black';
+      		red = true;
+			yellow = false;
+			green = false;
+      }
+}
+
+var timerA;
+function autoLight() {
+      timerA = window.setInterval(function() { lightChange(); }, 3000);
+}
+
+function startLight() {
+	red = true;
+	yellow = false;
+	green = false;
+	autoLight();
+}
+
+function stopLight() {
+      clearInterval(timerA);
+}
+
+
+autoLight();//call autolight on start
+
+}();//End of program
+
+
+
+
+
+
+
+
+
+
+
+
