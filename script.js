42fccc6d61695c9ce8059671ddec40d66d65fbeb
Matt Mobley <mattsmobley@gmail.com>
2015-11-12 15:03:46 -0700

    did this little stop-light event handler thang, BOSS

--- script.js
+++ script.js
@@ -8,3 +8,26 @@
   When I click on the 'go' button
     the bottom light should turn green.
 */
+
+var stop = document.getElementById('stopButton');
+stop.addEventListener("click", function onClick(){
+  document.querySelectorAll('.bulb')[2].style.backgroundColor = "black";
+  document.querySelectorAll('.bulb')[1].style.backgroundColor = "black";
+  document.querySelectorAll('.bulb')[0].style.backgroundColor = "red";
+});
+
+
+var slow = document.getElementById('slowButton');
+slow.addEventListener("click", function onClick(){
+  document.querySelectorAll('.bulb')[2].style.backgroundColor = "black";
+  document.querySelectorAll('.bulb')[0].style.backgroundColor = "black";
+  document.querySelectorAll('.bulb')[1].style.backgroundColor = "yellow";
+});
+
+
+var go = document.getElementById('goButton');
+go.addEventListener("click", function onClick(){
+  document.querySelectorAll('.bulb')[1].style.backgroundColor = "black";
+  document.querySelectorAll('.bulb')[0].style.backgroundColor = "black";
+  document.querySelectorAll('.bulb')[2].style.backgroundColor = "green";
+});
