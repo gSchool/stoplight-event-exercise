873a7e79750b80fc2b54cd2f6e6a5151adbc1338
bud aminof <budaminof@gmail.com>
2016-02-13 21:07:06 -0700

    JS code with out toggle.

--- script.js
+++ script.js
@@ -9,23 +9,45 @@
     the bottom light should turn green.
 */
 
-var stop = document.querySelector('#stopButton');
-var slow = document.querySelector('#slowButton');
-var go = document.querySelector('#goButton');
+// var stop = document.querySelector('#stopButton');
+// var slow = document.querySelector('#slowButton');
+// var go = document.querySelector('#goButton');
+//
+// stop.addEventListener('click', function () {
+// document.querySelector('#stopLight').style.backgroundColor= "rgb(246, 9, 37)";
+// ('#stopLight').toggle();
+// });
+//
+//
+// slow.addEventListener('click', function () {
+// document.querySelector('#slowLight').style.backgroundColor= "rgb(251, 166, 0)";
+// ('#slowLight').toggle();
+// });
+//
+//
+// go.addEventListener('click', function () {
+// document.querySelector('#goLight').style.backgroundColor= "rgb(11, 250, 49)";
+// ('#goLight').toggle();
+// });
 
-stop.addEventListener('click', function () {
-document.querySelector('#stopLight').style.backgroundColor= "rgb(246, 9, 37)";
-('#stopLight').toggle();
+var stopButton = document.getElementById('stopButton');
+var slowButton = document.getElementById('slowButton');
+var goButton = document.getElementById('goButton');
+
+stopButton.addEventListener('click', function(){
+  document.getElementById('stopLight').style.backgroundColor = "red";
+  document.getElementById('slowLight').style.backgroundColor = "black";
+  document.getElementById('goLight').style.backgroundColor = "black";
 });
 
-
-slow.addEventListener('click', function () {
-document.querySelector('#slowLight').style.backgroundColor= "rgb(251, 166, 0)";
-('#slowLight').toggle();
+slowButton.addEventListener('click', function(){
+  document.getElementById('slowLight').style.backgroundColor = "yellow";
+  document.getElementById('stopLight').style.backgroundColor = "black";
+  document.getElementById('goLight').style.backgroundColor = "black";
 });
 
-
-go.addEventListener('click', function () {
-document.querySelector('#goLight').style.backgroundColor= "rgb(11, 250, 49)";
-('#goLight').toggle();
+goButton.addEventListener('click', function(){
+  document.getElementById('goLight').style.backgroundColor = "green";
+  document.getElementById('stopLight').style.backgroundColor = "black";
+  document.getElementById('slowLight').style.backgroundColor = "black";
 });
