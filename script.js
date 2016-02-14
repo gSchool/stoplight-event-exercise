a69a15089d8fc88158231f9f00eb3899db60a9cc
bud aminof <budaminof@gmail.com>
2016-02-14 14:11:49 -0700

    this is the code i was trying to work with- i like this original idea better even though its not working..

--- script.js
+++ script.js
@@ -9,42 +9,42 @@
     the bottom light should turn green.
 */
 
+stop.addEventListener('click', function () {
+document.querySelector('#stopLight').style.backgroundColor= "rgb(246, 9, 37)";
+('#stopLight').toggle();
+});
+
+
+slow.addEventListener('click', function () {
+document.querySelector('#slowLight').style.backgroundColor= "rgb(251, 166, 0)";
+('#slowLight').toggle();
+});
+
+
+go.addEventListener('click', function () {
+document.querySelector('#goLight').style.backgroundColor= "rgb(11, 250, 49)";
+('#goLight').toggle();
+});
+
 var stop = document.querySelector('#stopButton');
 var slow = document.querySelector('#slowButton');
 var go = document.querySelector('#goButton');
 
 
-stop.addEventListener('click', function () {
-  document.querySelector('#stopLight').style.backgroundColor = "rgb(242, 13, 13)";
-  document.querySelector('#slowLight').style.backgroundColor = "rgb(0, 0, 0)";
-  document.querySelector('#goLight').style.backgroundColor = "rgb(0, 0, 0)";
-});
-
-slow.addEventListener('click', function () {
-  document.querySelector('#stopLight').style.backgroundColor = "rgb(0, 0, 0)";
-  document.querySelector('#slowLight').style.backgroundColor = "rgb(243, 255, 10)";
-  document.querySelector('#goLight').style.backgroundColor = "rgb(0, 0, 0)";
-});
-
-go.addEventListener('click', function () {
-  document.querySelector('#stopLight').style.backgroundColor = "rgb(0, 0, 0)";
-  document.querySelector('#slowLight').style.backgroundColor = "rgb(0, 0, 0)";
-  document.querySelector('#goLight').style.backgroundColor = "rgb(0, 255, 3)";
-});
-
 // stop.addEventListener('click', function () {
-// document.querySelector('#stopLight').style.backgroundColor= "rgb(246, 9, 37)";
-// ('#stopLight').toggle();
+//   document.querySelector('#stopLight').style.backgroundColor = "rgb(242, 13, 13)";
+//   document.querySelector('#slowLight').style.backgroundColor = "rgb(0, 0, 0)";
+//   document.querySelector('#goLight').style.backgroundColor = "rgb(0, 0, 0)";
 // });
-//
 //
 // slow.addEventListener('click', function () {
-// document.querySelector('#slowLight').style.backgroundColor= "rgb(251, 166, 0)";
-// ('#slowLight').toggle();
+//   document.querySelector('#stopLight').style.backgroundColor = "rgb(0, 0, 0)";
+//   document.querySelector('#slowLight').style.backgroundColor = "rgb(243, 255, 10)";
+//   document.querySelector('#goLight').style.backgroundColor = "rgb(0, 0, 0)";
 // });
 //
-//
 // go.addEventListener('click', function () {
-// document.querySelector('#goLight').style.backgroundColor= "rgb(11, 250, 49)";
-// ('#goLight').toggle();
+//   document.querySelector('#stopLight').style.backgroundColor = "rgb(0, 0, 0)";
+//   document.querySelector('#slowLight').style.backgroundColor = "rgb(0, 0, 0)";
+//   document.querySelector('#goLight').style.backgroundColor = "rgb(0, 255, 3)";
 // });
