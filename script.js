98c220d29e654b4704c92f1d4503c1d099f9e202
Daniel Stewart <daniel.robbins.stewart@gmail.com>
2016-02-13 19:47:59 -0700

    Add Javascript for working stoplight

--- script.js
+++ script.js
@@ -8,3 +8,25 @@
   When I click on the 'go' button
     the bottom light should turn green.
 */
+
+var stopButton = document.getElementById('stopButton');
+var slowButton = document.getElementById('slowButton');
+var goButton = document.getElementById('goButton');
+
+stopButton.addEventListener('click', function(){
+  document.getElementById('stopLight').style.backgroundColor = "red";
+  document.getElementById('slowLight').style.backgroundColor = "black";
+  document.getElementById('goLight').style.backgroundColor = "black";
+});
+
+slowButton.addEventListener('click', function(){
+  document.getElementById('slowLight').style.backgroundColor = "yellow";
+  document.getElementById('stopLight').style.backgroundColor = "black";
+  document.getElementById('goLight').style.backgroundColor = "black";
+});
+
+goButton.addEventListener('click', function(){
+  document.getElementById('goLight').style.backgroundColor = "green";
+  document.getElementById('stopLight').style.backgroundColor = "black";
+  document.getElementById('slowLight').style.backgroundColor = "black";
+});
