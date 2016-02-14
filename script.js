16b8e74d1a4abced6ab2fee9808ca1b9ce6d2047
Wfuger <wfuger@gmail.com>
2016-02-14 15:03:34 -0700

    event listeners for each button, lights change

--- script.js
+++ script.js
@@ -8,3 +8,19 @@
   When I click on the 'go' button
     the bottom light should turn green.
 */
+var stopButton = document.getElementById('stopButton');
+var slowButton = document.getElementById('slowButton');
+var goButton = document.getElementById('goButton');
+var stoplight = document.getElementById('stoplight');
+var slowLight = document.getElementById('slowLight');
+var goLight = document.getElementById('goLight');
+
+stopButton.addEventListener('click', function() {
+  stopLight.style.backgroundColor = 'red';
+})
+slowButton.addEventListener('click', function() {
+  slowLight.style.backgroundColor = 'orange';
+})
+goButton.addEventListener('click', function() {
+  goLight.style.backgroundColor = 'green';
+})
