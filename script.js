a6381e9b1ee8a6acaf8fe9080ad297d5667a1760
Madeline Pickering <madelinepickering@gmail.com>
2016-02-14 14:05:33 -0700

    toggled stoplights

--- script.js
+++ script.js
@@ -8,3 +8,21 @@
   When I click on the 'go' button
     the bottom light should turn green.
 */
+
+var stopbtn = document.getElementById('stopButton');
+var stoplight = document.getElementById('stopLight');
+stopbtn.addEventListener('click', function(){
+  stoplight.style.backgroundColor = (stoplight.style.backgroundColor == 'black') ? 'red' : 'black';
+  });
+
+var slowbtn = document.getElementById('slowButton');
+var slowlight = document.getElementById('slowLight');
+slowbtn.addEventListener('click', function(){
+  slowlight.style.backgroundColor = (slowlight.style.backgroundColor == 'black') ? 'orange' : 'black';
+  })
+
+var gobtn = document.getElementById('goButton');
+var golight = document.getElementById('goLight');
+gobtn.addEventListener('click', function(){
+  golight.style.backgroundColor = (golight.style.backgroundColor == 'black') ? 'green' : 'black';
+  })
