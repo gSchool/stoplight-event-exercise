563d27b53468c56fbb275d65e76130ea23b3f9b5
Mickey <develop.agility@gmail.com>
2016-02-15 15:13:38 -0700

    functional stop-light

--- script.js
+++ script.js
@@ -8,3 +8,50 @@
   When I click on the 'go' button
     the bottom light should turn green.
 */
+
+window.addEventListener('load', function(){
+
+
+  var btns = [document.getElementById('stopButton'),
+              document.getElementById('slowButton'),
+              document.getElementById('goButton')
+  ];
+
+  var lights = [document.getElementById('stopLight'),
+                document.getElementById('slowLight'),
+                document.getElementById('goLight')
+  ];
+
+
+  btns.forEach(function(x){
+
+    x.addEventListener('click', function(){
+
+      lights.forEach(function(y){
+
+        y.style.backgroundColor = 'black';
+
+      });
+    });
+  });
+
+
+  btns[0].addEventListener('click', function(){
+
+    lights[0].style.backgroundColor = 'red';
+
+  });
+
+  btns[1].addEventListener('click', function(){
+
+    lights[1].style.backgroundColor = 'yellow';
+
+  });
+
+  btns[2].addEventListener('click', function(){
+
+    lights[2].style.backgroundColor = 'lightgreen';
+
+  });
+
+});
