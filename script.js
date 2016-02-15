0a4343ac0e6b9fbcea77063a795b43a70aef13b2
nickmac <nickpmacintyre@gmail.com>
2016-02-15 15:02:26 -0700

    toggles lights when clicking new button

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-/*
-  Write JS to make this stoplight work.
+var button = document.getElementsByClassName('button');
+var bulb = document.getElementsByClassName('bulb');
+var color = ['red', 'yellow', 'green'];
 
-  When I click on the 'stop' button,
-    the top light should turn red.
-  When I click on the 'slow' button
-    the middle light should turn orange.
-  When I click on the 'go' button
-    the bottom light should turn green.
-*/
+function toggle(x) {
+  for (var i = 0; i < button.length; i++) {
+    bulb[i].style.backgroundColor = '#111';
+  }
+  bulb[x].style.backgroundColor = color[x];
+}
+
+button[0].addEventListener('click', function(){toggle(0)});
+button[1].addEventListener('click', function(){toggle(1)});
+button[2].addEventListener('click', function(){toggle(2)});
