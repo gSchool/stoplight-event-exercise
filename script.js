cd00f19f78e7a4b78ee3c3900211114266ea9b6d
Alan Taylor <alancarltaylor@gmail.com>
2016-02-15 14:02:49 -0700

    stoplight works well, but does not toggle

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+var stopButton = document.getElementById("stopButton");
+var slowButton = document.getElementById("slowButton");
+var goButton = document.getElementById("goButton");
+
+stopButton.addEventListener("click", stopLight);
+slowButton.addEventListener("mousedown", slowLight);
+goButton.addEventListener("mousedown", goLight);
+
+function stopLight(){
+  var idTargetStop = document.getElementById("stopLight")
+    //if (idTargetStop.style.backgroundColor.value = "red"){
+      idTargetStop.style.backgroundColor = "red"
+//} else {idTargetStop.style.backgroundColor.value = "red"}
+
+}
+function slowLight(){
+  var idTargetSlow = document.getElementById("slowLight")
+  idTargetSlow.style.backgroundColor = "yellow";
+}
+function goLight(){
+  var idTargetGo = document.getElementById("goLight")
+  idTargetGo.style.backgroundColor = "green";
+}
+
+
+
+
+
+
+
+
 /*
   Write JS to make this stoplight work.
 
