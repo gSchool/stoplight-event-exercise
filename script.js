f103141af87c865b193bccf868eb2ecdbcac4a71
Mike Homolak <mhomolak@yahoo.com>
2016-02-14 16:23:07 -0800

    finished traffic light

--- script.js
+++ script.js
@@ -8,3 +8,23 @@
   When I click on the 'go' button
     the bottom light should turn green.
 */
+var stopbtn = document.getElementById('stopButton');
+stopbtn.addEventListener('click', function() {
+  document.getElementById('stopLight').style.backgroundColor = 'red';
+  document.getElementById('slowLight').style.backgroundColor = 'black';
+  document.getElementById('goLight').style.backgroundColor = 'black';
+});
+
+var slowbtn = document.getElementById('slowButton');
+slowbtn.addEventListener('click', function() {
+  document.getElementById('slowLight').style.backgroundColor = 'orange';
+  document.getElementById('stopLight').style.backgroundColor = 'black';
+  document.getElementById('goLight').style.backgroundColor = 'black';
+});
+
+var gobtn = document.getElementById('goButton');
+gobtn.addEventListener('click', function() {
+  document.getElementById('goLight').style.backgroundColor = 'green';
+  document.getElementById('stopLight').style.backgroundColor = 'black';
+  document.getElementById('slowLight').style.backgroundColor = 'black';  
+});
