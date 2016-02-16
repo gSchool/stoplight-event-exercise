30fe5fa54525e5226e3f0fb8ded6ecda9402b796
MSturges <Sturges3232@gmail.com>
2016-02-16 12:38:07 -0700

    Stop light changes colors

--- script.js
+++ script.js
@@ -8,3 +8,33 @@
   When I click on the 'go' button
     the bottom light should turn green.
 */
+
+
+
+ var stopButton = document.getElementById('stopButton');
+ var slowButton = document.getElementById('slowButton');
+ var gotButton = document.getElementById('goButton');
+
+ var stopLight = document.getElementById('stopLight');
+ var slowLight = document.getElementById('slowLight');
+ var goLight = document.getElementById('goLight');
+
+ stopButton.addEventListener('click', function() {
+   stopLight.style.backgroundColor = 'red';
+   slowLight.style.backgroundColor = 'black';
+   goLight.style.backgroundColor = 'black';
+
+ });
+
+slowButton.addEventListener('click', function() {
+  slowLight.style.backgroundColor = 'yellow';
+  stopLight.style.backgroundColor = 'black';
+  goLight.style.backgroundColor = 'black';
+
+  });
+
+goButton.addEventListener('click', function() {
+  goLight.style.backgroundColor = 'green';
+  stopLight.style.backgroundColor = 'black';
+  slowLight.style.backgroundColor = 'black';
+})
