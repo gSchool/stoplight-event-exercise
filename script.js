4ccb8ea1ed8c2aab6e814e0706314b0bc3e9261f
Sean Stokke <sstokke17@gmail.com>
2016-02-22 13:30:02 -0800

    add toggling buttons

--- script.js
+++ script.js
@@ -11,20 +11,50 @@
 
 
 
-function redClick () {
- stopLight.style = "background-color: red";
+// function redClick () {
+//  stopLight.style = "background-color: red";
+// }
+//
+// stopButton.addEventListener('click', redClick)
+//
+// function orangeClick () {
+//  slowLight.style = "background-color: orange";
+// }
+//
+// slowButton.addEventListener('click', orangeClick)
+//
+// function greenClick () {
+//  goLight.style = "background-color: green";
+// }
+//
+// goButton.addEventListener('click', greenClick)
+
+function toggleRed() {
+    var clicked = document.getElementById('stopLight')
+    if (clicked.className == 'bulb') {
+      clicked.className = 'bulbRed';
+    } else {
+      clicked.className = 'bulb';
+    };
 }
+stopButton.addEventListener("click", toggleRed);
 
-stopButton.addEventListener('click', redClick)
-
-function orangeClick () {
- slowLight.style = "background-color: orange";
+function toggleOrange() {
+    var clicked = document.getElementById('slowLight')
+    if (clicked.className == 'bulb') {
+      clicked.className = 'bulbOrange';
+    } else {
+      clicked.className = 'bulb';
+    };
 }
+slowButton.addEventListener("click", toggleOrange);
 
-slowButton.addEventListener('click', orangeClick)
-
-function greenClick () {
- goLight.style = "background-color: green";
+function toggleGreen() {
+    var clicked = document.getElementById('goLight')
+    if (clicked.className == 'bulb') {
+      clicked.className = 'bulbGreen';
+    } else {
+      clicked.className = 'bulb';
+    };
 }
-
-goButton.addEventListener('click', greenClick)
+goButton.addEventListener("click", toggleGreen);
