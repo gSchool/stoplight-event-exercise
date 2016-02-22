4f9d4fd0fb873d07127f779f62f8bd61810334c6
Sean Stokke <sstokke17@gmail.com>
2016-02-22 14:23:04 -0800

    cleaned up js

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-/*
-  Write JS to make this stoplight work.
-
-  When I click on the 'stop' button,
-    the top light should turn red.
-  When I click on the 'slow' button
-    the middle light should turn orange.
-  When I click on the 'go' button
-    the bottom light should turn green.
-*/
-
-
-
-// function redClick () {
-//  stopLight.style = "background-color: red";
-// }
-//
-// stopButton.addEventListener('click', redClick)
-//
-// function orangeClick () {
-//  slowLight.style = "background-color: orange";
-// }
-//
-// slowButton.addEventListener('click', orangeClick)
-//
-// function greenClick () {
-//  goLight.style = "background-color: green";
-// }
-//
-// goButton.addEventListener('click', greenClick)
-
 function toggleRed() {
     var clicked = document.getElementById('stopLight')
     if (clicked.className == 'bulb') {
