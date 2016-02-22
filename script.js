9736bd8b730a6370dcb8d5a9c3e165f26ec75626
derzorngottes <amy.conchie@gmail.com>
2016-02-22 11:38:32 -0800

    Add code to toggle stoplight buttons

--- script.js
+++ script.js
@@ -8,3 +8,37 @@
   When I click on the 'go' button
     the bottom light should turn green.
 */
+var stopBtn = document.getElementById("stopButton");
+var slowBtn = document.getElementById("slowButton");
+var goBtn = document.getElementById("goButton");
+var stopLt = document.getElementById("stopLight");
+var slowLt = document.getElementById("slowLight");
+var goLt = document.getElementById("goLight");
+
+
+stopBtn.addEventListener('click', function() {
+  if(stopLt.style.background=="red"){
+    stopLt.style.background="black";
+  }
+  else{
+    stopLt.style.background="red";
+  }
+});
+
+slowBtn.addEventListener('click', function(){
+  if(slowLt.style.background=="yellow"){
+    slowLt.style.background="black";
+  }
+  else{
+    slowLt.style.background="yellow";
+  }
+})
+
+goBtn.addEventListener('click', function() {
+  if(goLt.style.background=="green"){
+    goLt.style.background="black";
+  }
+  else{
+    goLt.style.background="green";
+  }
+})
