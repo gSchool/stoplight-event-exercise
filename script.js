1022c82ec9e84e209f3f37b22ebf241e1738c7a2
Diana Kim <kimdiana@hotmail.com>
2016-02-24 12:02:21 -0800

    Lint traffic light js

--- script.js
+++ script.js
@@ -8,62 +8,61 @@
   When I click on the 'go' button
     the bottom light should turn green.
 */
+(function(){
+var stop = document.getElementById('stopButton');
+var slow = document.getElementById('slowButton');
+var go = document.getElementById('goButton');
 
-var stop = document.getElementById("stopButton");
-var slow = document.getElementById("slowButton");
-var go = document.getElementById("goButton");
+var stopLight = document.getElementById('stopLight');
+var slowLight = document.getElementById('slowLight');
+var goLight = document.getElementById('goLight');
 
-var stopLight = document.getElementById("stopLight");
-var slowLight = document.getElementById("slowLight");
-var goLight = document.getElementById("goLight");
-
-function clickedStop(){
-    stopLight.style.backgroundColor = "#B81811";
-    goLight.style.backgroundColor = "#2d2d2d";
-    slowLight.style.backgroundColor = "#2d2d2d";
+var clickedStop = function() {
+    stopLight.style.backgroundColor = '#B81811';
+    goLight.style.backgroundColor = '#2d2d2d';
+    slowLight.style.backgroundColor = '#2d2d2d';
   };
-function clickedSlow(){
-    slowLight.style.backgroundColor = "#FFED18";
-    goLight.style.backgroundColor = "#2d2d2d";
-    stopLight.style.backgroundColor = "#2d2d2d";
+var clickedSlow = function() {
+    slowLight.style.backgroundColor = '#FFED18';
+    goLight.style.backgroundColor = '#2d2d2d';
+    stopLight.style.backgroundColor = '#2d2d2d';
   };
-function clickedGo(){
-    goLight.style.backgroundColor = "#348F37";
-    slowLight.style.backgroundColor = "#2d2d2d";
-    stopLight.style.backgroundColor = "#2d2d2d";
+var clickedGo = function() {
+    goLight.style.backgroundColor = '#348F37';
+    slowLight.style.backgroundColor = '#2d2d2d';
+    stopLight.style.backgroundColor = '#2d2d2d';
   };
 
 stop.addEventListener("click", clickedStop);
 go.addEventListener("click", clickedGo);
 slow.addEventListener("click", clickedSlow);
-
+}()
 
 //KYLE'S CODE
-
-var button1 = document.getElementById("stopButton");
-var light = document.getElementById("stopLight");
-button1.addEventListener('mouseenter', function(){
-light.style.backgroundColor = 'red';
-});
-button1.addEventListener('mouseleave', function(){
-light.style.backgroundColor = 'black';
-});
-
-
-var button2 = document.getElementById("slowButton");
-var light2 = document.getElementById("slowLight");
-button2.addEventListener('mouseenter', function(){
-light2.style.backgroundColor = 'yellow';
-});
-button2.addEventListener('mouseleave', function(){
-light2.style.backgroundColor = 'black';
-});
-
-var button3 = document.getElementById("goButton");
-var light3 = document.getElementById("goLight");
-button3.addEventListener('mouseenter', function(){
-light3.style.backgroundColor = 'green';
-});
-button3.addEventListener('mouseleave', function(){
-light3.style.backgroundColor = 'black';
-});
+// var button1 = document.getElementById("stopButton");
+// var light = document.getElementById("stopLight");
+// button1.addEventListener('mouseenter', function(){
+// light.style.backgroundColor = 'red';
+// });
+// button1.addEventListener('mouseleave', function(){
+// light.style.backgroundColor = 'black';
+// });
+//
+//
+// var button2 = document.getElementById("slowButton");
+// var light2 = document.getElementById("slowLight");
+// button2.addEventListener('mouseenter', function(){
+// light2.style.backgroundColor = 'yellow';
+// });
+// button2.addEventListener('mouseleave', function(){
+// light2.style.backgroundColor = 'black';
+// });
+//
+// var button3 = document.getElementById("goButton");
+// var light3 = document.getElementById("goLight");
+// button3.addEventListener('mouseenter', function(){
+// light3.style.backgroundColor = 'green';
+// });
+// button3.addEventListener('mouseleave', function(){
+// light3.style.backgroundColor = 'black';
+// });
