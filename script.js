4815c9fa4b4c4ab662301cedb64a1be7c094bad7
jordan-storz <jordan.storz.1@gmail.com>
2016-10-31 15:45:48 -0600

    Console logs on button hover

--- script.js
+++ script.js
@@ -8,3 +8,11 @@
   When I click on the 'go' button
     the bottom light should turn green.
 */
+var stopBtn = $('#stopButton');
+var slowBtn = $('#slowButton');
+var goBtn = $('#goButton');
+
+$('.button').mouseover(function() {
+  var text = $(this).text();
+  console.log(`Entered ${text} button`);
+})
