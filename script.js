ae5a2e3a7cd7beee9760cdd68cbc475535af7706
jeremyclayton <jeremy.clayton.91@gmail.com>
2016-11-03 16:49:25 -0600

    beautified code

--- script.js
+++ script.js
@@ -9,76 +9,79 @@
     the bottom light should turn green.
 */
 
-document.getElementById('stopButton').addEventListener('mouseover', function (){
+document.getElementById('stopButton').addEventListener('mouseover', function() {
 
     console.log(document.getElementById('stopLight').text = 'Entered ' + document.getElementById('stopButton').textContent + ' button');
 });
-document.getElementById('stopButton').addEventListener('mouseout', function (){
+document.getElementById('stopButton').addEventListener('mouseout', function() {
     console.log(document.getElementById('stopLight').text = 'Left ' + document.getElementById('stopButton').textContent + ' button');
 });
 //slow button functions
-document.getElementById('slowButton').addEventListener('mouseover', function (){
+document.getElementById('slowButton').addEventListener('mouseover', function() {
 
     console.log(document.getElementById('stopLight').text = 'Entered ' + document.getElementById('slowButton').textContent + ' button');
 });
-document.getElementById('slowButton').addEventListener('mouseout', function (){
+document.getElementById('slowButton').addEventListener('mouseout', function() {
     console.log(document.getElementById('stopLight').text = 'Left ' + document.getElementById('slowButton').textContent + ' button');
 });
 //go button functions
-document.getElementById('goButton').addEventListener('mouseover', function (){
+document.getElementById('goButton').addEventListener('mouseover', function() {
 
     console.log(document.getElementById('goLight').text = 'Entered ' + document.getElementById('goButton').textContent + ' button');
 });
-document.getElementById('goButton').addEventListener('mouseout', function (){
+document.getElementById('goButton').addEventListener('mouseout', function() {
     console.log(document.getElementById('goLight').text = 'Left ' + document.getElementById('goButton').textContent + ' button');
 });
-document.getElementById('controls').addEventListener('click', function (){
+document.getElementById('controls').addEventListener('click', function() {
     console.log(event.target.textContent);
 });
 // toggling if one is off then all others are off
-document.getElementById('stopButton').addEventListener('click', function(){
-    if (document.getElementById('stopLight').style.background === 'red'){
-    document.getElementById('stopLight').style.background = 'black';
-} else  {document.getElementById('stopLight').style.background = 'red';
-}
+document.getElementById('stopButton').addEventListener('click', function() {
+    if (document.getElementById('stopLight').style.background === 'red') {
+        document.getElementById('stopLight').style.background = 'black';
+    } else {
+        document.getElementById('stopLight').style.background = 'red';
+    }
 });
-document.getElementById('slowButton').addEventListener('click', function(){
-    if (document.getElementById('slowLight').style.background === 'yellow'){
-    document.getElementById('slowLight').style.background = 'black';
-} else  {document.getElementById('slowLight').style.background = 'yellow';
-}
+document.getElementById('slowButton').addEventListener('click', function() {
+    if (document.getElementById('slowLight').style.background === 'yellow') {
+        document.getElementById('slowLight').style.background = 'black';
+    } else {
+        document.getElementById('slowLight').style.background = 'yellow';
+    }
 });
-document.getElementById('goButton').addEventListener('click', function(){
-    if (document.getElementById('goLight').style.background === 'green'){
-    document.getElementById('goLight').style.background = 'black';
-} else  {document.getElementById('goLight').style.background = 'green';
-}
+document.getElementById('goButton').addEventListener('click', function() {
+    if (document.getElementById('goLight').style.background === 'green') {
+        document.getElementById('goLight').style.background = 'black';
+    } else {
+        document.getElementById('goLight').style.background = 'green';
+    }
 });
-document.getElementById('stopButton').addEventListener('click', function(){
-    if (document.getElementById('slowLight').style.background === 'yellow'){
-    document.getElementById('slowLight').style.background = 'black';}
- else if (document.getElementById('goLight').style.background === 'green'){
+document.getElementById('stopButton').addEventListener('click', function() {
+    if (document.getElementById('slowLight').style.background === 'yellow') {
+        document.getElementById('slowLight').style.background = 'black';
+    } else if (document.getElementById('goLight').style.background === 'green') {
         document.getElementById('goLight').style.background = 'black';
     }
 });
-document.getElementById('stopButton').addEventListener('click', function(){
-    if (document.getElementById('slowLight').style.background === 'yellow'){
-    document.getElementById('slowLight').style.background = 'black';}
- else if (document.getElementById('goLight').style.background === 'green'){
+document.getElementById('stopButton').addEventListener('click', function() {
+    if (document.getElementById('slowLight').style.background === 'yellow') {
+        document.getElementById('slowLight').style.background = 'black';
+    } else if (document.getElementById('goLight').style.background === 'green') {
         document.getElementById('goLight').style.background = 'black';
     }
 });
-document.getElementById('slowButton').addEventListener('click', function(){
-    if (document.getElementById('stopLight').style.background === 'red'){
-    document.getElementById('stopLight').style.background = 'black';}
- else if (document.getElementById('goLight').style.background === 'green'){
+document.getElementById('slowButton').addEventListener('click', function() {
+    if (document.getElementById('stopLight').style.background === 'red') {
+        document.getElementById('stopLight').style.background = 'black';
+    } else if (document.getElementById('goLight').style.background === 'green') {
         document.getElementById('goLight').style.background = 'black';
     }
 });
-document.getElementById('goButton').addEventListener('click', function(){
-    if (document.getElementById('slowLight').style.background === 'yellow'){
-    document.getElementById('slowLight').style.background = 'black';}
- else if (document.getElementById('stopLight').style.background === 'red'){
+document.getElementById('goButton').addEventListener('click', function() {
+    if (document.getElementById('slowLight').style.background === 'yellow') {
+        document.getElementById('slowLight').style.background = 'black';
+    } else if (document.getElementById('stopLight').style.background === 'red') {
         document.getElementById('stopLight').style.background = 'black';
     }
 });
