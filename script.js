3f52adee538d678950165d2de2304898fd6f3a86
Chana Reynolds <chana777@gmail.com>
2016-11-03 15:49:54 -0600

    Green-Means-Go ls

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+$(document).ready(function() {
+    $('#stopButton').on('click', function() {
+        $('#stopLight').css('background-color', 'red');
+    })
+    $('#slowButton').on('click', function() {
+        $('#slowLight').css('background-color', 'yellow');
+    })
+    $('#goButton').on('click', function() {
+        $('#goLight').css('background-color', 'green');
+    })
+})
+
+
+
 /*
   Write JS to make this stoplight work.
-
   When I click on the 'stop' button,
     the top light should turn red.
   When I click on the 'slow' button
@@ -8,3 +21,6 @@
   When I click on the 'go' button
     the bottom light should turn green.
 */
+// $('button').button(function makeRed() {
+//     $(makeRed).css('color', 'red');
+// })
