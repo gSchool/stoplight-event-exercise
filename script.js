10ba93993277249170930c083e57903ac136136f
Tyler Briskie <tylerbriskie@gmail.com>
2016-11-04 10:42:37 -0600

    finished stoplight exercise, and bonus

--- script.js
+++ script.js
@@ -13,8 +13,17 @@ var stopButton = document.getElementById('stopButton');
 stopButton.addEventListener('click', function(){
   if (stopLight.classList.contains('red')){
     stopLight.classList.remove('red');
-  } else {
+  } else if (goLight.classList.contains('green')){
+    goLight.classList.remove('green');
+    slowLight.classList.add('yellow');
+    setTimeout(function(){
+      slowLight.classList.remove('yellow');
+      stopLight.classList.add('red');
+    }, 5000);
+  }else {
     stopLight.classList.add('red');
+    slowLight.classList.remove('yellow');
+    goLight.classList.remove('green');
   }
 });
 
@@ -27,9 +36,12 @@ slowButton.addEventListener('click', function(){
   if (slowLight.classList.contains('yellow')){
     slowLight.classList.remove('yellow');
   } else {
+    stopLight.classList.remove('red');
     slowLight.classList.add('yellow');
+    goLight.classList.remove('green');
   }
 });
+
 slowButton.addEventListener('mouseover', function(){
   console.log("Entered " + slowButton.innerHTML + " button.");
 });
@@ -39,9 +51,12 @@ goButton.addEventListener('click', function(){
   if (goLight.classList.contains('green')){
     goLight.classList.remove('green');
   } else {
+    stopLight.classList.remove('red');
+    slowLight.classList.remove('yellow');
     goLight.classList.add('green');
   }
 });
+
 goButton.addEventListener('mouseover', function(){
   console.log("Entered " + goButton.innerHTML + " button.");
 });
