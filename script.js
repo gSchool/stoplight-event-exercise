38ccc2a477e8dc125d035f612275fe59f6fca4a7
Matthew Winzer <matthewwinzer@Matthews-MacBook-Air.local>
2016-11-06 16:13:51 -0700

    added button mouseover listeners and functions

--- script.js
+++ script.js
@@ -8,3 +8,29 @@
   When I click on the 'go' button
     the bottom light should turn green.
 */
+
+$(document).ready(function() {
+  $('#stopButton').mouseover(mouseIn);
+  $('#slowButton').mouseover(mouseIn);
+  $('#goButton').mouseover(mouseIn);
+
+  $('#stopButton').mouseout(mouseOut);
+  $('#slowButton').mouseout(mouseOut);
+  $('#goButton').mouseout(mouseOut);
+
+  $('.button').click(buttonClick);
+
+
+});
+
+function mouseIn(event) {
+  console.log('Entered <textContent> button');
+}
+
+function mouseOut(event) {
+  console.log('Left <textContent> button');
+}
+
+function buttonClick(event) {
+  console.log($('.button').text());
+}
