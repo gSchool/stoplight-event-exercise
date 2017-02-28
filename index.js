8030bfba91ad64f156c6213bdc6383c301bd99ae
TOBY A GODFREY <toby@TOBYs-MacBook-Pro.local>
2017-02-28 08:32:46 -0600

    Initial Commit

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 (function() {
-  'use strict';
+    'use strict';
+    // YOUR CODE HERE
+    var redButton = $('#stopButton');
+    var yellowButton = $('#slowButton');
+    var greenButton = $('#goButton');
 
-  // YOUR CODE HERE
+
+    redButton.on('click', function(ev) {
+        // $('#stopLight.bulb').bgColor = "red";
+        $('#stopLight.bulb').css("background-color", "red");
+        $('#slowLight.bulb').css("background-color", "black");
+        $('#goLight.bulb').css("background-color", "black");
+    });
+
+    yellowButton.on('click', function(ev) {
+        // $('#stopLight.bulb').bgColor = "red";
+        $('#slowLight.bulb').css("background-color", "yellow");
+        $('#stopLight.bulb').css("background-color", "black");
+        $('#goLight.bulb').css("background-color", "black");
+    });
+
+    greenButton.on('click', function(ev) {
+        // $('#stopLight.bulb').bgColor = "red";
+        $('#goLight.bulb').css("background-color", "green");
+        $('#slowLight.bulb').css("background-color", "black");
+        $('#stopLight.bulb').css("background-color", "black");
+    });
+
+
+
 })();
