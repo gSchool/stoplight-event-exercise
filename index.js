bbbc7262bb59854f9f78bffd7cb88b4a83cae714
James Proett <jamesproett@JAP.local>
2017-02-28 19:24:07 -0600

    g45 stoplight

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
-(function() {
-  'use strict';
+'use strict';
+$(function() {
 
-  // YOUR CODE HERE
-})();
+    var buttonsss = document.getElementById('controls')
+    var theBulbs = document.getElementsByClassName('bulb')
+    // console.log(theBulbs.);
+
+    buttonsss.addEventListener("click", function(ev) {
+        console.log(ev.target);
+        $('#stopLight').css({
+            backgroundColor: '#111'
+        })
+        $('#slowLight').css({
+            backgroundColor: '#111'
+        })
+        $('#goLight').css({
+            backgroundColor: '#111'
+        })
+        switch (ev.target.textContent) {
+            case 'Stop':
+                return document.getElementById('stopLight').style.backgroundColor = 'red';
+                break;
+            case 'Slow':
+                return document.getElementById('slowLight').style.backgroundColor = 'orange';
+                break;
+            case 'Go':
+                return document.getElementById('goLight').style.backgroundColor = 'green';
+                break;
+            default:
+                theBulbs.style.backgroundColor = '#111'
+        }
+
+    })
+
+});
