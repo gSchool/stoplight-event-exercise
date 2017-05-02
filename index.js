97676aebce4009d1ef8c51f64556a4b184de02a0
Will Carpenter <willcarpenteriii@gmail.com>
2017-05-01 18:34:53 -0600

    part-one done

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  // YOUR CODE HERE
+    // buttons
+    var stopButton = document.getElementById('stopButton'),
+        slowButton = document.getElementById('slowButton'),
+        goButton = document.getElementById('goButton');
+
+
+    // bulbs
+    var stopLight = document.getElementById('stopLight'),
+        slowLight = document.getElementById('slowLight'),
+        goLight = document.getElementById('goLight');
+
+
+
+    stopButton.addEventListener('click', function() {
+        setColor(stopLight);
+    });
+
+    slowButton.addEventListener('click', function() {
+        setColor(slowLight);
+    });
+
+    goButton.addEventListener('click', function() {
+        setColor(goLight);
+    });
+
+
+
+    function setColor(bulb) {
+        if (bulb == stopLight) {
+            bulb.style.backgroundColor= '#ff0000';
+        } else if(bulb == slowLight) {
+            bulb.style.backgroundColor = '#ffb90f';
+        } else {
+            bulb.style.backgroundColor = '#00b300';
+        }
+
+    }
+
 })();
