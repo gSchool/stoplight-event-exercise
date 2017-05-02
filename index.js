9797041d9b9b835a2e15594782ef3ffa479fd2f1
Will Carpenter <willcarpenteriii@gmail.com>
2017-05-01 19:51:43 -0600

    finished stoplight problems, not bonus yet

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
 (function() {
     'use strict';
 
-    // buttons
+    // buttons by id
     var stopButton = document.getElementById('stopButton'),
         slowButton = document.getElementById('slowButton'),
         goButton = document.getElementById('goButton');
 
 
-    // bulbs
+    // the lights
     var stopLight = document.getElementById('stopLight'),
         slowLight = document.getElementById('slowLight'),
         goLight = document.getElementById('goLight');
+
+    // buttons class
+
+    var btns = document.getElementsByClassName('button');
+
+    btns[0].onmouseover = function() {
+        console.log(btns[0].textContent);
+    };
+
+    btns[1].onmouseover = function() {
+        console.log(btns[1].textContent);
+    };
+
+    btns[2].onmouseover = function() {
+        console.log(btns[2].textContent);
+    };
+
+
+
+    //
+    // function showButtonText() {
+    //     for(var i = 0; i < btns.length; i++) {
+    //         if()
+    //     }
+    // }
+    //
+    // showButtonText();
+    //
+    //
+
 
 
 
@@ -29,14 +59,20 @@
 
 
     function setColor(bulb) {
-        if (bulb == stopLight) {
-            bulb.style.backgroundColor= '#ff0000';
-        } else if(bulb == slowLight) {
-            bulb.style.backgroundColor = '#ffb90f';
-        } else {
-            bulb.style.backgroundColor = '#00b300';
-        }
 
+        if (bulb == stopLight) {
+
+            bulb.style.backgroundColor= '#ff0000';
+
+        } else if(bulb == slowLight) {
+
+            bulb.style.backgroundColor = '#ffb90f';
+
+        } else {
+
+            bulb.style.backgroundColor = '#00b300';
+
+        }
     }
 
 })();
