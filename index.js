5a6b15f0f12761259fae64ea84cbabba70a7207a
Andrew Eskenazi <Drewesk@gmail.com>
2017-05-06 11:23:37 -0600

    Completed Part 1, First Commit!
    * Buttons toggle the correct color when click event fires.

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 (function() {
   'use strict';
+  // Lights
+let stop = document.getElementById('stopLight');
+let slow = document.getElementById('slowLight');
+let go = document.getElementById('goLight');
+  //Buttons
+let stopButton = document.getElementById('stopButton');
+let slowButton = document.getElementById('slowButton');
+let goButton = document.getElementById('goButton');
 
-  // YOUR CODE HERE
+stopButton.addEventListener('click', function(){
+  toggleLight(stop, 'stop');
+});
+
+slowButton.addEventListener('click', function(){
+  toggleLight(slow, 'slow');
+});
+
+goButton.addEventListener('click', function(){
+  toggleLight(go, 'go');
+});
+
+function toggleLight(light, className) {
+  light.classList.toggle(className);
+};
+
 })();
