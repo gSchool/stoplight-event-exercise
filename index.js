e3bcedbf9933e5d3390fe8895438ea1d1551b87b
WILL MEIER <willmeierart@gmail.com>
2017-05-06 16:47:04 -0600

    complete

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 (function() {
   'use strict';
+  $('#stopButton').mouseover(function(){
 
-  // YOUR CODE HERE
+      console.log('Entered stop button')
+  }).click(function(){
+      $('#stopLight').toggleClass('stop')
+  })
+
+  $('#slowButton').mouseover(function(){
+      console.log('Entered slow button')
+  }).click(function(){
+      $('#slowLight').toggleClass('slow')
+  })
+
+  $('#goButton').mouseover(function(){
+      console.log('Entered go button')
+  }).click(function(){
+      $('#goLight').toggleClass('go')
+  })
 })();
