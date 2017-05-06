ee445c156a86ff8e1801d8373d7ab0054422a7b2
JAKEFELDMAN <jacob.h.feldman@gmail.com>
2017-05-05 22:26:16 -0600

    Finished Stoplight assessment

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+
 (function() {
   'use strict';
-
-  // YOUR CODE HERE
+  document.getElementById("stopButton").addEventListener("click", function() {
+    document.getElementById("stopLight").classList.toggle('stop');
+  });
+  document.getElementById("slowButton").addEventListener("click", function() {
+    document.getElementById("slowLight").classList.toggle('slow');
+  });
+  document.getElementById("goButton").addEventListener("click", function() {
+    document.getElementById("goLight").classList.toggle('go');
+  });
 })();
