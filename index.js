611a28c7ab58682d839994c5e8f878b85a6f439f
JAKEFELDMAN <jacob.h.feldman@gmail.com>
2017-05-06 14:37:41 -0600

    Updated with variables to clean up document

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
 
 (function() {
   'use strict';
-  document.getElementById("stopButton").addEventListener("click", function() {
-    document.getElementById("stopLight").classList.toggle('stop');
+  var stopButton = document.getElementById("stopButton");
+  var stopLight = document.getElementById("stopLight");
+  var slowButton = document.getElementById("slowButton");
+  var slowLight = document.getElementById("slowLight");
+  var goButton = document.getElementById("goButton");
+  var goLight = document.getElementById("goLight");
+
+  stopButton.addEventListener("click", function() {
+    stopLight.classList.toggle('stop');
   });
-  document.getElementById("stopButton").addEventListener("mouseenter", function() {
-    console.log("Entered stop button");
+  stopButton.addEventListener("mouseenter", function() {
+    console.log("Entered " + stopButton.textContent + " Button");
   });
-  document.getElementById("stopButton").addEventListener("mouseleave", function() {
-    console.log("Leave stop button");
+  stopButton.addEventListener("mouseleave", function() {
+    console.log("Leave " + stopButton.textContent + " Button");
   });
 
-  document.getElementById("slowButton").addEventListener("click", function() {
-    document.getElementById("slowLight").classList.toggle('slow');
+  slowButton.addEventListener("click", function() {
+    slowLight.classList.toggle('slow');
   });
-  document.getElementById("slowButton").addEventListener("mouseenter", function() {
-    console.log("Entered stop button");
+  slowButton.addEventListener("mouseenter", function() {
+    console.log("Entered " + slowButton.textContent + " Button");
   });
-  document.getElementById("slowButton").addEventListener("mouseleave", function() {
-    console.log("Leave stop button");
-  });
-
-  document.getElementById("goButton").addEventListener("click", function() {
-    document.getElementById("goLight").classList.toggle('go');
-  });
-  document.getElementById("goButton").addEventListener("mouseenter", function() {
-    console.log("Entered go button");
-  });
-  document.getElementById("goButton").addEventListener("mouseleave", function() {
-    console.log("Leave go button");
+  slowButton.addEventListener("mouseleave", function() {
+    console.log("Leave " + slowButton.textContent + " Button");
   });
 
+  goButton.addEventListener("click", function() {
+    goLight.classList.toggle('go');
+  });
+  goButton.addEventListener("mouseenter", function() {
+    console.log("Entered " + goButton.textContent + " Button");
+  });
+  goButton.addEventListener("mouseleave", function() {
+    console.log("Leave " + goButton.textContent + " Button");
+  });
 })();
