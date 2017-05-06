34826f5ff2644c1a888d5eb7565fa1882c8c3b7f
JAKEFELDMAN <jacob.h.feldman@gmail.com>
2017-05-05 22:36:21 -0600

    Finished part 2 assessment

--- index.js
+++ index.js
@@ -4,10 +4,31 @@
   document.getElementById("stopButton").addEventListener("click", function() {
     document.getElementById("stopLight").classList.toggle('stop');
   });
+  document.getElementById("stopButton").addEventListener("mouseenter", function() {
+    console.log("Entered stop button");
+  });
+  document.getElementById("stopButton").addEventListener("mouseleave", function() {
+    console.log("Leave stop button");
+  });
+
   document.getElementById("slowButton").addEventListener("click", function() {
     document.getElementById("slowLight").classList.toggle('slow');
   });
+  document.getElementById("slowButton").addEventListener("mouseenter", function() {
+    console.log("Entered slow button");
+  });
+  document.getElementById("slowButton").addEventListener("mouseleave", function() {
+    console.log("Leave slow button");
+  });
+
   document.getElementById("goButton").addEventListener("click", function() {
     document.getElementById("goLight").classList.toggle('go');
   });
+  document.getElementById("goButton").addEventListener("mouseenter", function() {
+    console.log("Entered go button");
+  });
+  document.getElementById("goButton").addEventListener("mouseleave", function() {
+    console.log("Leave go button");
+  });
+
 })();
