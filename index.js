063504275f96c312a885cd3ce193e71c084c5481
Andrew Eskenazi <Drewesk@gmail.com>
2017-05-06 11:42:58 -0600

    Part 2 Complete!

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 (function() {
   'use strict';
+  // Part 1
+
   // Lights
+
 let stop = document.getElementById('stopLight');
 let slow = document.getElementById('slowLight');
 let go = document.getElementById('goLight');
+
   //Buttons
+
 let stopButton = document.getElementById('stopButton');
 let slowButton = document.getElementById('slowButton');
 let goButton = document.getElementById('goButton');
@@ -23,6 +28,44 @@ goButton.addEventListener('click', function(){
 
 function toggleLight(light, className) {
   light.classList.toggle(className);
-};
+}
+
+//Part 2
+
+//console log mouseenter
+
+stopButton.addEventListener('mouseenter', function(){
+  logMouseEnter('stop');
+});
+
+slowButton.addEventListener('mouseenter', function(){
+  logMouseEnter('slow');
+});
+
+goButton.addEventListener('mouseenter', function(){
+  logMouseEnter('go');
+});
+
+function logMouseEnter(light) {
+  console.log('Entered ' + light + ' button');
+}
+
+//console log mouseleave
+
+stopButton.addEventListener('mouseleave', function(){
+  logMouseLeave('stop');
+});
+
+slowButton.addEventListener('mouseleave', function(){
+  logMouseLeave('slow');
+});
+
+goButton.addEventListener('mouseleave', function(){
+  logMouseLeave('go');
+});
+
+function logMouseLeave(light) {
+  console.log('Left ' + light + ' button');
+}
 
 })();
